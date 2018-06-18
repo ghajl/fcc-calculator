2fb217ad0a7047c9c92931d5944470ded81e4b1b
ghajl <michaelhnn@gmail.com>
2018-06-19 01:27:09 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -3,91 +3,91 @@ import ReactDOM from 'react-dom';
 import './scss/main.scss';
 
 const Button = props => {
-	return (
-		<div className="button"></div>
-	)
+  return (
+    <div className="button"></div>
+  )
 }
 
 const Display = props => {
-	return (
-		<div className="display"></div>
-	)
+  return (
+    <div className="display"></div>
+  )
 }
 
 class Calculator extends Component {
 
-	render() {
-		return (
-			<div className="case">
-				<div className="display-wrapper">
-					<Display />
-				</div>
-				<div className="controls">
-					<div className="row">
-						<div className="button-wrapper align-end">
-							<Button />
-						</div>
-					</div>	
-					<div className="row">
-						<div className="button-wrapper">
-							<Button />
-						</div>
-						<div className="button-wrapper">
-							<Button />
-						</div>
-						<div className="button-wrapper">
-							<Button />
-						</div>
-						<div className="button-wrapper">
-							<Button />
-						</div>
-					</div>
-					<div className="row">
-						<div className="button-wrapper">
-							<Button />
-						</div>
-						<div className="button-wrapper">
-							<Button />
-						</div>
-						<div className="button-wrapper">
-							<Button />
-						</div>
-						<div className="button-wrapper">
-							<Button />
-						</div>
-					</div>
-					<div className="row">
-						<div className="button-wrapper">
-							<Button />
-						</div>
-						<div className="button-wrapper">
-							<Button />
-						</div>
-						<div className="button-wrapper">
-							<Button />
-						</div>
-						<div className="button-wrapper">
-							<Button />
-						</div>
-					</div>
-					<div className="row">
-						<div className="button-wrapper">
-							<Button />
-						</div>
-						<div className="button-wrapper">
-							<Button />
-						</div>
-						<div className="button-wrapper">
-							<Button />
-						</div>
-						<div className="button-wrapper">
-							<Button />
-						</div>
-					</div>															
-				</div>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className="case">
+        <div className="display-wrapper">
+          <Display />
+        </div>
+        <div className="controls">
+          <div className="row">
+            <div className="button-wrapper align-end">
+              <Button />
+            </div>
+          </div>  
+          <div className="row">
+            <div className="button-wrapper">
+              <Button />
+            </div>
+            <div className="button-wrapper">
+              <Button />
+            </div>
+            <div className="button-wrapper">
+              <Button />
+            </div>
+            <div className="button-wrapper">
+              <Button />
+            </div>
+          </div>
+          <div className="row">
+            <div className="button-wrapper">
+              <Button />
+            </div>
+            <div className="button-wrapper">
+              <Button />
+            </div>
+            <div className="button-wrapper">
+              <Button />
+            </div>
+            <div className="button-wrapper">
+              <Button />
+            </div>
+          </div>
+          <div className="row">
+            <div className="button-wrapper">
+              <Button />
+            </div>
+            <div className="button-wrapper">
+              <Button />
+            </div>
+            <div className="button-wrapper">
+              <Button />
+            </div>
+            <div className="button-wrapper">
+              <Button />
+            </div>
+          </div>
+          <div className="row">
+            <div className="button-wrapper">
+              <Button />
+            </div>
+            <div className="button-wrapper">
+              <Button />
+            </div>
+            <div className="button-wrapper">
+              <Button />
+            </div>
+            <div className="button-wrapper">
+              <Button />
+            </div>
+          </div>                              
+        </div>
+      </div>
+    )
+  }
 }
 
 const Root = (props) => {
