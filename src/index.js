21ae34b9bd1e9e16f60045a2af7411ee8dce74d0
ghajl <michaelhnn@gmail.com>
2018-06-20 21:04:39 +0300

    Implement arithmetic

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const Button = props => {
 
 const Display = props => {
   return (
-    <div className="display">{props.output}</div>
+    <div className="display"><span>{props.output}</span></div>
   )
 }
 
@@ -20,49 +20,201 @@ class Calculator extends Component {
     output: '0',
   }
 
-  control = {
-    zero: this.createControl('zero', '0', () => this.getSymbol('0')),
-    one: this.createControl('one', '1', () => this.getSymbol('1')),
-    two: this.createControl('two', '2', () => this.getSymbol('2')),
-    three: this.createControl('three', '3', () => this.getSymbol('3')),
-    four: this.createControl('four', '4', () => this.getSymbol('4')),
-    five: this.createControl('five', '5', () => this.getSymbol('5')),
-    six: this.createControl('six', '6', () => this.getSymbol('6')),
-    seven: this.createControl('seven', '7', () => this.getSymbol('7')),
-    eight: this.createControl('eight', '8', () => this.getSymbol('8')),
-    nine: this.createControl('nine', '9', () => this.getSymbol('9')),
-    decimal: this.createControl('decimal', '.', () => this.getSymbol('.')),
-    add: this.createControl('add', '+', () => this.getSymbol('+')),
-    subtract: this.createControl('subtract', '-', () => this.getSymbol('-')),
-    multiply: this.createControl('multiply', '*', () => this.getSymbol('*')),
-    divide: this.createControl('divide', '/', () => this.getSymbol('/')),
-    equals: this.createControl('equals', '=', () => this.getSymbol('=')),
+  expression = '0';
+
+  specsign = {
+    'multiply': String.fromCharCode(215),
+    'divide': String.fromCharCode(247),
+  }
+  
+  button = {
+    zero: this.createControl('zero', '0', () => this.handleInput('zero')),
+    one: this.createControl('one', '1', () => this.handleInput('one')),
+    two: this.createControl('two', '2', () => this.handleInput('two')),
+    three: this.createControl('three', '3', () => this.handleInput('three')),
+    four: this.createControl('four', '4', () => this.handleInput('four')),
+    five: this.createControl('five', '5', () => this.handleInput('five')),
+    six: this.createControl('six', '6', () => this.handleInput('six')),
+    seven: this.createControl('seven', '7', () => this.handleInput('seven')),
+    eight: this.createControl('eight', '8', () => this.handleInput('eight')),
+    nine: this.createControl('nine', '9', () => this.handleInput('nine')),
+    decimal: this.createControl('decimal', '.', () => this.handleInput('decimal')),
+    add: this.createControl('add', '+', () => this.handleInput('add')),
+    subtract: this.createControl('subtract', '-', () => this.handleInput('subtract')),
+    multiply: this.createControl('multiply', '*', () => this.handleInput('multiply')),
+    divide: this.createControl('divide', '/', () => this.handleInput('divide')),
+    equals: this.createControl('equals', '=', () => this.compute()),
     clear: this.createControl('clear', 'C', () => this.clear()),
   }
 
-  createControl(key, label, action) {
-    return {key, label, action};
+  createControl(key, symbol, action) {
+    return {key, symbol, action};
   }
 
-  getSymbol = (key) => {
-    let expression = this.state.output;
-    if (/[1-9]/.test(key) && /(.*[+\-\*\/]|^)0$/.test(expression)) {
-      expression = expression.slice(0, expression.length - 1) + key
-    } else if (key == '.' && /.*[+\-\*\/]$/.test(expression)) {
-      expression = expression + '0.';
-    } else if (/[+\-\*\/]/.test(key) && /.*[+\-\*\/]$/.test(expression)) {
-      expression = expression.slice(0, expression.length - 1) + key
+  handleInput = (key) => {
+    let symbol = this.button[key].symbol;
+    let output = this.state.output;
+    if (output === 'error') {
+      output = '0';
+    }
+    if (/[1-9]/.test(symbol) && /(.*[+\-\*\/]|^)0$/.test(this.expression)) {//remove zero
+      this.expression = this.expression.slice(0, this.expression.length - 1) + symbol;
+      output = output.slice(0, output.length - 1) + symbol;
+    } else if (symbol == '.' && /.*[+\-\*\/]$/.test(this.expression)) { //insert zero before decimal
+      this.expression += '0.';
+      output += '0.';
+    } else if (/[+\-\*\/]/.test(symbol) && /.*[+\-\*\/]$/.test(this.expression)) { //replace arithmetic signs
+      this.expression = this.expression.slice(0, this.expression.length - 1) + symbol;
+      if (key === 'multiply' || key === 'divide') {
+        output = output.slice(0, output.length - 1) + this.specsign[key];
+        
+      } else {
+        output = output.slice(0, output.length - 1) + symbol;
+      }
     } else {
-      if (this.isValid(expression, key)) {
-        expression = expression + key; 
+      if (this.isValid(this.expression, symbol)) {
+        this.expression += symbol; 
+        if (key === 'multiply' || key === 'divide') {
+          output += this.specsign[key];
+        } else {
+          output += symbol;
+        }
       }
     }
     
-    this.setState({output: expression});
+    this.setState({output});
   }
 
   clear = () => {
+    this.expression = '0';
     this.setState({output: '0'});
+  }
+
+  compute = () => {
+    const expressionRoot = this.parse(this.expression);
+
+    const result = this.getResult(expressionRoot);
+    if (result == 'error') {
+      this.expression = '0';
+    } else {
+      this.expression = result;
+    }
+    this.setState({output: result});
+  }
+
+
+  /*
+   *return treeNode = {
+    value: number or 'add',...
+    type: 'number' or 'action'
+    left: treeNode,
+    right: treeNode,
+   } 
+   */
+  parse(expression) {
+    const re = /\d+(?:\.\d+)?/g;
+
+    const treeNode = function(value) {
+      this.value = value;
+      this.type = null;
+      this.left = null;
+      this.right = null;
+    }
+
+    const tree = {
+      root: null,
+
+      insertNumber: function(num) {
+        const newNode = new treeNode(num);
+        newNode.type = 'number';
+        if (this.root === null) {
+          this.root = newNode;
+        } else {
+          const parent = (function findNode(node){
+            if (node.right == null) {
+              return node;
+            } 
+            return findNode(node.right);
+          })(this.root);
+          parent.right = newNode;
+        }
+      },
+
+      insertAction: function(action) {
+        const newNode = new treeNode(action);
+        newNode.type = 'action';
+        if (this.root === null)  {
+          throw 'invalid operation';
+        }
+        if (
+          (this.root.type === 'number')
+          || (action === '+' || action === '-')
+          || (this.root.value === '*' || this.root.value === '/')
+        ) {
+          newNode.left = this.root;
+          this.root = newNode;
+        } else {
+          newNode.left = this.root.right;
+          this.root.right = newNode;
+        }
+      },
+    }
+
+    let token;
+
+    while ((token = re.exec(expression)) !== null) {
+      const currentNumber = Number(token);
+      if (Number.isNaN(currentNumber)) {
+        throw 'invalid operation';
+      }
+      tree.insertNumber(currentNumber);
+      if (re.lastIndex === expression.length) {
+        return tree.root;
+      }
+      const symbol = expression.charAt(re.lastIndex);
+      tree.insertAction(symbol);
+    }
+    return tree.root;
+  }
+
+  getResult(root) {
+
+    const result = (function getValue(node) {
+      if (node.type === 'number') {
+        return node.value;
+      }
+      const leftValue = getValue(node.left);
+      const rightValue = getValue(node.right);
+      if (rightValue === 'error' || leftValue === 'error') {
+        return 'error';
+      }
+      let res;
+      switch (node.value) {
+        case '+': {
+          res = leftValue + rightValue;
+          break;
+        }
+        case '-': {
+          res = leftValue - rightValue;
+          break;
+        }
+        case '*': {
+          res = leftValue * rightValue;
+          break;
+        }
+        case '/': {
+          if (rightValue === 0) {
+            res = 'error';
+          } else {
+            res = leftValue / rightValue;
+          }
+          
+          break;
+        }
+      }
+      return res;
+    })(root);
+    return result;
   }
 
   isValid(expression, symbol) {
@@ -72,12 +224,15 @@ class Calculator extends Component {
   }
 
   renderButton(type) {
-    const {key, label, action} = this.control[type];
+    let {key, symbol, action} = this.button[type];
+    if (key === 'multiply' || key === 'divide') {
+      symbol = this.specsign[key];
+    }
     return (
       <div className="button-wrapper">
         <Button 
           id={key} 
-          label={label}
+          label={symbol}
           handleClick={action}
         />
       </div>
